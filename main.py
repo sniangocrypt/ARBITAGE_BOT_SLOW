4d0f3c8375a2a5cb9327a931e63e932f11370a4b
sniangocrypt <denis0007000700070007@gmail.com>
2024-10-27 23:24:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import json
 
 tokens = ["BTC","ETH","SOL","XLM","DOGE"]
 tokenss = tokens[1]
+
 # ПОЛУЧЕМ ЦЕНУ С КУКОИНА
 
 SYMBOL = f"{tokenss}-USDT"
@@ -20,9 +21,11 @@ for token in r["data"]["ticker"]:
 	price = token["last"]
 	if symbol == SYMBOL:
 		print(Fore.GREEN +f"Цена  нa куоине {price}")
-		price_kukoin = {price}
+		price_kukoin = float(price)
 	with open("venv\kukoin.json", "w", encoding="utf-8") as file:
 		json.dump(price, file, ensure_ascii=False, indent=4)
+
+# ПОЛУЧЕМ ЦЕНУ С БИНАНСА
 
 SYMBOL_BINANCE = {
   "symbol": f"{tokenss}USDT",
@@ -32,16 +35,17 @@ url = "https://api.binance.com/api/v3/ticker/price"
 params = SYMBOL_BINANCE
 response_binance = requests.get(url= url, params = params)
 BINANCE = response_binance.json()
-price_binane = {price}
+price_binane = float(BINANCE['price'])
 print(f"Цена на биненсе {BINANCE['price']}")
 with open("venv\BINANCE.json", "w", encoding="utf-8") as file:
 	json.dump(price, file, ensure_ascii=False, indent=4)
 
 
-
+print(price_kukoin/price_binane)
 if (price_kukoin/price_binane) >= 1.05:
 	print(f"Разница в цене {tokenss} KUKOIN/BINANCE = {(price_kukoin/price_binane)}")
 elif (price_binane/price_kukoin) >= 1.05:
 	print(f"Разница в цене {tokenss} BINANCE/KUKOIN = {(price_binane/price_kukoin)}")
 else:
 	print(f"Арбтиража нет БЛЯТЬ, ищем дальше...")
+
